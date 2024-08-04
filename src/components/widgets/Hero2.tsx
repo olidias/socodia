64b3ebdc2e341295d1988a23d05a774a020b2612
Oli Dias <info@dscs.io>
2024-08-04 15:32:59 +0200

    more detailed seo, add alt tag

--- src/components/widgets/Hero2.tsx
+++ src/components/widgets/Hero2.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { WavyBackground } from "./WavyBackground.tsx";
 export interface HeroProps {
   image: string;
-  subtitle:string; 
+  subtitle: string;
 }
 export function Hero2(props: HeroProps) {
 
@@ -11,9 +11,9 @@ export function Hero2(props: HeroProps) {
   return (
     <WavyBackground className="max-w-4xl mx-auto">
       <div className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center my-10 mx-7 md:mx-auto">
-      {
+        {
           props.image && (
-            <img src={props.image} className="min-h-[15vh]" />
+            <img src={props.image} className="min-h-[15vh]" alt="socodia logo" />
           )
         }      </div>
       <p className="sm:mx-1 text-2xl dark:text-gray-300 text-muted mb-6 self-center text-center text-slate-700 align-middle flex justify-center w-[280px] xl:w-[350px]">
